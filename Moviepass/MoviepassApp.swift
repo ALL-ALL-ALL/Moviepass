5663a23883cb05db1dfb8f0347f6dec053229ead
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-03-07 01:36:53 +0100

    Initial Commit

---
+++ Moviepass/MoviepassApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviepassApp.swift
+//  Moviepass
+//
+//  Created by  Ixart on 07/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviepassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
