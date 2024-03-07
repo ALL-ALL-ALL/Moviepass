f79ba9da97b76aafb7034983ee40867e8837031b
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-03-07 01:44:18 +0100

    en cours ....

--- Moviepass/ContentView.swift
+++ Moviepass/ContentView.swift
@@ -10,14 +10,30 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+            Text("Ed Catmull.")
+                .italic()
+
+            Text("Creativity,Inc.")
+                .font(.title)
+                .foregroundStyle(.black)
+
+        } // FIN VSTACK
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    } // FIN BODY
+} // FIN STRu=ct
 
 #Preview {
     ContentView()
